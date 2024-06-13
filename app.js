ff91b9917078f4deda3e2bf1381826b4b22592fd
P-Sanjana <podduturisanjana@gmail.com>
2024-06-13 12:08:24 -0400

    added initial files and configuration 🎉

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const passport = require('passport')
+const session = require('express-session')
+
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' })
+
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.engine('.hbs', exphbs.engine({defayltLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', require('./routes/index'))
+
+const port = process.env.port;
+
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
