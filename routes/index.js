7604ca4cc4f8aa6bcab2aba314d248efa4aad88e
P-Sanjana <podduturisanjana@gmail.com>
2024-06-15 22:03:43 -0400

    added auth middleware 🛂

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 })
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
     res.render('dashboard')
 })
 
